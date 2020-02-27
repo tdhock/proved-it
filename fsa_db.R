eac67495946f5886b20cfd1261a910c3e5b48a28
Toby Dylan Hocking <tdhock5@gmail.com>
2020-02-27 08:15:54 -0700

    parse treatments

--- fsa_db.R
+++ fsa_db.R
@@ -58,6 +58,64 @@ suffix.pattern <- list(
   "[.]",
   injection.seconds="[0-9]+", as.integer,
   "sec")
+extra.pattern <- list(
+  single.pattern,
+  "|",
+  mixture.pattern)
+single.pattern <- list(
+  identifier="[0-9]+")
+mixture.pattern <- list(
+  identifiers.str="[0-9_]+",
+  "-",
+  ratios.str="[0-9;]+",
+  "-")
+alternatives <- function(...){
+  in.list <- list(...)
+  stopifnot(1 < length(in.list))
+  out.list <- in.list[1]
+  for(i in 2:length(in.list)){
+    out.list[[length(out.list)+1L]] <- "|"
+    out.list[[length(out.list)+1L]] <- in.list[[i]]
+  }
+  out.list
+}
+single.or.mixture <- alternatives(
+  single.pattern,
+  mixture.pattern)
+treatment.pattern <- alternatives(
+  "[a-e]",
+  list("-", fragmentase.minutes="[0-9]+"),
+  list("U", uv.minutes="[0-9]+"),
+  list("S", sonication.cycles="[0-9]+"),
+  list("I", acid.microliters="[0-9]+"))
+treatment.symbol.names <- c(
+  S="sonication",
+  I="humic.acid",
+  U="ultraviolet",
+  "-"="fragmentase",
+  a="none")
+DNase.units <- c(b=3, c=6, d=12, e=24)
+treatment.symbol.names[names(DNase.units)] <- "DNase"
+treatment.name.units <- c(
+  DNase="mU",
+  fragmentase="minutes",
+  ultraviolet="minutes",
+  sonication="cycles",
+  humic.acid="μL")
+treatment.pattern <- list(
+  treatment.symbol="[-a-eUSI]",
+  treatment.amount=nc::quantifier("[0-9]+", "?"), as.integer)
+maybe.number.treatment <- nc::quantifier(
+  dilution.number="[0-9]+", as.integer,
+  treatment=nc::quantifier(treatment.pattern, "?"),
+  "?")
+maybe.dilution <- nc::quantifier(
+  dilution.letter="[dM]",
+  maybe.number.treatment,
+  "?")
+extra.pattern <- list(
+  single.or.mixture,
+  maybe.dilution)
 fsa.pattern <- list(
   prefix.pattern,
   lane=list(
@@ -65,10 +123,7 @@ fsa.pattern <- list(
     "|",
     project=project.pattern,
     "-",
-    extra=list(
-      single.pattern,
-      "|",
-      mixture.pattern)),
+    extra=extra.pattern),
   suffix.pattern)
 subject.vec <- c(
   "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(080114LEA_10sec)/A01-Ladder-PP16-001.10sec.fsa",
@@ -77,10 +132,18 @@ subject.vec <- c(
   "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(080114LEA_10sec)/A02-RD12-0002-35-0.5PP16-001.10sec.fsa",
   "data/PROVEDIt_1-Person Profiles_3130 10sec_IDPlus28cycles/10 sec/RD14-0003(032516LEA_10sec)/G01_RD14-0003-35d3S30-0.01563P-Q10.0_003.10sec.fsa",
   "data/PROVEDIt_1-Person Profiles_3130 10sec_IDPlus28cycles/10 sec/RD14-0003(011816CMG_10sec)/A06_RD14-0003-24d3a-0.0625IP-Q0.8_001.10sec.fsa",
+  "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(090514LEA_10sec)/A08-RD12-0002-01d-0.125PP16-001.10sec.fsa",
+  "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(090514LEA_10sec)/A10-RD12-0002-04d1-0.0625PP16-001.10sec.fsa",
+  "data/PROVEDIt_1-Person Profiles_3130 5sec_IDPlus28cycles/5 sec/RD14-0003(121715CMG_5sec)/C02_RD14-0003-15d2b-0.25IP-Q0.5_003.5sec.fsa",
   ## 1 above 2-5 below.
   "data/PROVEDIt_2-5-Person Profiles_3130 5sec_IDPlus28cycles/5sec/RD14-0003(051616LEA_5sec)/H07_RD14-0003-35_36_37_38_39-1;4;4;4;1-M2I35-0.75IP-QLAND_004.5sec.fsa",
   "data/PROVEDIt_2-5-Person Profiles_3130 5sec_IDPlus28cycles/5sec/RD14-0003(051616LEA_5sec)/D07_Ladder-IP_004.5sec.fsa")
-nc::capture_first_vec(subject.vec, fsa.pattern)
+(test.dt <- nc::capture_first_vec(subject.vec, fsa.pattern))
+test.dt[, treatment.name := treatment.symbol.names[treatment.symbol] ]
+test.dt[, treatment.units := treatment.name.units[treatment.name] ]
+test.dt[
+  treatment.symbol %in% names(DNase.units),
+  treatment.amount := DNase.units[treatment.symbol] ]
 
 match.dt <- nc::capture_first_vec(fsa.vec, fsa.pattern)
 fsa_db <- data.table(fsa=fsa.vec, match.dt)
