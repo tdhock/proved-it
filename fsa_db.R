c605f86cb7c78681a82ee470ea4a91021e15371b
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2020-02-27 09:22:28 -0700

    new regex parses treatment for all

--- fsa_db.R
+++ fsa_db.R
@@ -9,33 +9,13 @@ match.dt[, table(persons)]
 
 prefix.pattern <- list(
   "/",
-  letter="[A-Z]",
-  numbers="[0-9]+",
+  well.letter="[A-Z]",
+  well.number="[0-9]+", as.integer,
   "[-_]")
-mixture.pattern <- list(
-  identifiers.str="[^-]+", # within that project.
-  "-",
-  ratios.str="[^-]+",
-  nc::quantifier("-", mix.dilution="M[^-]+", "?"))
 project.pattern <- list(
   "RD[0-9]+",
   "-",
   "[0-9]+")
-single.pattern <- list(
-  ## It is to be noted that each sample is designated by the
-  ## combination of project number and sample identifier. For example,
-  ## RD14-0003-21 will have a different known genotype than
-  ## RD12-0002-21.
-  sample.identifier="[0-9]+", as.integer, # within that project.
-  ## d_ is the dilution number which was used by laboratory personnel
-  ## to distinguish between extracts.
-  nc::quantifier(
-    "d",
-    single.dilution=".*?",
-    ## the ‘x’ designator indicates DNA condition (Supplementary Methods
-    ## Table 1)
-    DNA.condition="x?",
-    "?"))
 nanograms.pattern <- nc::quantifier(
   template.nanograms="[0-9.]+", as.numeric, "?")
 suffix.pattern <- list(
@@ -54,21 +34,21 @@ suffix.pattern <- list(
   ## The designator 002 is the capillary number, where capillaries are
   ## numbered 001-004 on the 3130 electrophoresis platform and 01-08
   ## on the 3500 platform.
-  capillary="[0-9]+",
+  capillary="[0-9]+", as.integer,
   "[.]",
   injection.seconds="[0-9]+", as.integer,
   "sec")
-extra.pattern <- list(
-  single.pattern,
-  "|",
-  mixture.pattern)
 single.pattern <- list(
-  identifier="[0-9]+")
+  ## It is to be noted that each sample is designated by the
+  ## combination of project number and sample identifier. For example,
+  ## RD14-0003-21 will have a different known genotype than
+  ## RD12-0002-21.
+  sample.identifier="[0-9]+", as.integer)  #within that project.
 mixture.pattern <- list(
   identifiers.str="[0-9_]+",
   "-",
   ratios.str="[0-9;]+",
-  "-")
+  "-?")
 alternatives <- function(...){
   in.list <- list(...)
   stopifnot(1 < length(in.list))
@@ -82,12 +62,6 @@ alternatives <- function(...){
 single.or.mixture <- alternatives(
   single.pattern,
   mixture.pattern)
-treatment.pattern <- alternatives(
-  "[a-e]",
-  list("-", fragmentase.minutes="[0-9]+"),
-  list("U", uv.minutes="[0-9]+"),
-  list("S", sonication.cycles="[0-9]+"),
-  list("I", acid.microliters="[0-9]+"))
 treatment.symbol.names <- c(
   S="sonication",
   I="humic.acid",
@@ -106,6 +80,8 @@ treatment.pattern <- list(
   treatment.symbol="[-a-eUSI]",
   treatment.amount=nc::quantifier("[0-9]+", "?"), as.integer)
 maybe.number.treatment <- nc::quantifier(
+  ## d_ is the dilution number which was used by laboratory personnel
+  ## to distinguish between extracts.
   dilution.number="[0-9]+", as.integer,
   treatment=nc::quantifier(treatment.pattern, "?"),
   "?")
@@ -125,6 +101,25 @@ fsa.pattern <- list(
     "-",
     extra=extra.pattern),
   suffix.pattern)
+capture_convert <- function(fsa){
+  m.dt <- nc::capture_first_vec(fsa, fsa.pattern)
+  m.dt[, treatment.name := treatment.symbol.names[treatment.symbol] ]
+  m.dt[, treatment.units := treatment.name.units[treatment.name] ]
+  m.dt[
+    treatment.symbol %in% names(DNase.units),
+    treatment.amount := DNase.units[treatment.symbol] ]
+  m.dt[, type := data.table::fcase(
+    control != "", "control",
+    sample.identifier != "", "single",
+    identifiers.str != "", "mixture",
+    default="unknown")]
+  m.dt[, Q.num := as.numeric(ifelse(Q=="LAND", 0, Q))]
+  data.table(fsa, m.dt[, .(
+    well.letter, well.number, control, project, sample.identifier,
+    identifiers.str, ratios.str, dilution.letter, dilution.number,
+    treatment.name, treatment.amount, treatment.units,
+    template.nanograms, kit, Q.num, capillary, injection.seconds, type)])
+}
 subject.vec <- c(
   "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(080114LEA_10sec)/A01-Ladder-PP16-001.10sec.fsa",
   "data/PROVEDIt_1-Person Profiles_3130 10sec_IDPlus28cycles/10 sec/RD14-0003(120715CMG_10sec)/A12_RB121514ADG_001.10sec.fsa",
@@ -136,15 +131,12 @@ subject.vec <- c(
   "data/PROVEDIt_1-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(090514LEA_10sec)/A10-RD12-0002-04d1-0.0625PP16-001.10sec.fsa",
   "data/PROVEDIt_1-Person Profiles_3130 5sec_IDPlus28cycles/5 sec/RD14-0003(121715CMG_5sec)/C02_RD14-0003-15d2b-0.25IP-Q0.5_003.5sec.fsa",
   ## 1 above 2-5 below.
+  "data/PROVEDIt_2-5-Person Profiles_3130 10sec_PP16HS32cycles/10sec/RD12-0002(102414LEA_10sec)/A02-RD12-0002-1_2-1;9-0.125PP16-001.10sec.fsa",
   "data/PROVEDIt_2-5-Person Profiles_3130 5sec_IDPlus28cycles/5sec/RD14-0003(051616LEA_5sec)/H07_RD14-0003-35_36_37_38_39-1;4;4;4;1-M2I35-0.75IP-QLAND_004.5sec.fsa",
   "data/PROVEDIt_2-5-Person Profiles_3130 5sec_IDPlus28cycles/5sec/RD14-0003(051616LEA_5sec)/D07_Ladder-IP_004.5sec.fsa")
-(test.dt <- nc::capture_first_vec(subject.vec, fsa.pattern))
-test.dt[, treatment.name := treatment.symbol.names[treatment.symbol] ]
-test.dt[, treatment.units := treatment.name.units[treatment.name] ]
-test.dt[
-  treatment.symbol %in% names(DNase.units),
-  treatment.amount := DNase.units[treatment.symbol] ]
+(test.dt <- capture_convert(subject.vec))
 
-match.dt <- nc::capture_first_vec(fsa.vec, fsa.pattern)
+(match.dt <- capture_convert(fsa.vec))
+
 fsa_db <- data.table(fsa=fsa.vec, match.dt)
 fwrite(fsa_db, "fsa_db.csv")
